9ba3d2d420965c5a010fca8745bf3c426b406e11
SujitDeshmukh45 <deshmukhsujit1999@gmail.com>
2023-12-08 09:51:52 +0530

    fisrt commit

---
+++ Demo.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Demo {
+    public static void main(String[] args) {
+        List l=new Demo();
+        l.add(10);
+        l.add(20);
+        l.add(1,15);
+        System.out.println(l);
+    }
+}
